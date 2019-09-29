4e580f52b6098201da4630fdca70c2f9ad26da88
Faraaz-Beyabani <beyabanifaraaz@gmail.com>
2019-09-29 18:42:32 -0500

    Catalog aesthetics complete

--- src/App.js
+++ src/App.js
@@ -5,7 +5,35 @@ import 'firebase/database';
 import db from "./components/index.js";
 
 import 'rbx/index.css';
-import {Card, Column, Notification} from 'rbx';
+import {Card, Column, Image, Level, Content, Button, Divider} from 'rbx';
+
+const ProductCard = ({ product }) => {
+  return (
+    <Card key={product.sku}>
+      <Card.Image>
+        <Image.Container>
+          <Image src={require('../public/data/products/'+product.sku+'_1.jpg')}/>
+        </Image.Container>
+      </Card.Image>
+      <Card.Content align="center">
+        <Content style={{fontSize:"17px"}}>
+          {product.title}
+          <Divider style={{marginTop:"12px", marginBottom:"12px"}}/>
+          ${parseFloat(product.price).toFixed(2)}
+        </Content>
+      </Card.Content>
+      <Card.Footer>
+        <Card.Footer.Item>
+          <Button.Group>
+            {["S","M","L","XL"].map(size => (
+              <Button>{size}</Button>
+            ))}
+          </Button.Group>
+        </Card.Footer.Item>
+      </Card.Footer>
+    </Card>
+  )
+}
 
 const App = () => {
   const [data, setData] = useState({});
@@ -20,13 +48,13 @@ const App = () => {
   }, []);
 
   return (
-    // <ul>
-    //   {products.map(product => <Card key={product.sku}>{product.title}</Card>)}
-    // </ul>
-    <Column.Group>
+    <Column.Group style={{marginTop:"10px", marginLeft:"20px", marginRight:"20px"}}>
       {[1, 2, 3, 4].map(i => (
         <Column key={i}>          
-            {products.slice(4*(i-1), 4*i).map(product => <Card key={product.sku}>{product.title}</Card>)}
+            {products.slice(4*(i-1), 4*i).map(product => 
+            <Level style={{display:"flex"}}>
+              <ProductCard product={product}/>
+            </Level>)}
         </Column>
       ))}
     </Column.Group>
